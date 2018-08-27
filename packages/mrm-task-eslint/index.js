a915a6ceb954e07dfb8f946159a72d56854b7cd9
Liucw <liucw.mail@gmail.com>
2018-08-27 21:50:24 +0800

    feat(task-eslint): .eslintrc -> .eslintrc.yml

--- packages/mrm-task-eslint/index.js
+++ packages/mrm-task-eslint/index.js
@@ -1,116 +1,125 @@
-const { json, packageJson, lines, install, uninstall, getExtsFromCommand } = require('mrm-core');
+const { json, yaml, packageJson, lines, install, uninstall, getExtsFromCommand } = require('mrm-core')
 
-function task(config) {
-	let exts = '';
-	const ignores = ['node_modules/'];
-	const gitIgnores = ['.eslintcache'];
-	const packages = [
-		'eslint',
-		'eslint-config-standard',
-		'eslint-plugin-standard',
-		'eslint-plugin-promise',
-		'eslint-plugin-import',
-		'eslint-plugin-node'
-	];
-	const oldPackages = ['jslint', 'jshint'];
-	const { eslintPreset, eslintPeerDependencies, eslintObsoleteDependencies, eslintRules, eslintEnv } = config
-		.defaults({
-			eslintPreset: 'standard',
-			eslintPeerDependencies: [],
-			eslintObsoleteDependencies: [],
-			eslintRules: {},
-			eslintEnv: {
-				es6: true,
-				node: true,
-			}
-		})
-		.values();
+function task (config) {
+  let exts = ''
+  const ignores = ['node_modules/']
+  const gitIgnores = ['.eslintcache']
+  const packages = [
+    'eslint',
+    'eslint-config-standard',
+    'eslint-plugin-standard',
+    'eslint-plugin-promise',
+    'eslint-plugin-import',
+    'eslint-plugin-node',
+  ]
+  const oldPackages = ['jslint', 'jshint']
+  const { eslintPreset, eslintPeerDependencies, eslintObsoleteDependencies, eslintRules, eslintEnv } = config
+    .defaults({
+      eslintPreset: 'standard',
+      eslintPeerDependencies: [],
+      eslintObsoleteDependencies: [],
+      eslintRules: {
+        'no-console': 1,
+        'object-curly-spacing': [2, 'always'],
+        'comma-dangle': [2, 'always-multiline'],
+      },
+      eslintEnv: {
+        es6: true,
+        node: true,
+      },
+    })
+    .values()
 
-	// Preset
-	if (eslintPreset !== 'standard') {
-		packages.push(`eslint-config-${eslintPreset}`);
-	}
+  // Preset
+  if (eslintPreset !== 'standard') {
+    packages.push(`eslint-config-${eslintPreset}`)
+  }
 
-	// Peer dependencies
-	packages.push(...eslintPeerDependencies);
+  // Peer dependencies
+  packages.push(...eslintPeerDependencies)
 
-	// .eslintrc
-	const eslintrc = json('.eslintrc');
-	if (!eslintrc.get('extends', '').startsWith(eslintPreset)) {
-		eslintrc.set('extends', eslintPreset);
-	}
-	if (eslintEnv) {
-		eslintrc.merge({ env: eslintEnv });
-	}
-	if (eslintRules) {
-		eslintrc.merge({ rules: eslintRules });
-	}
+  // .eslintrc -> .eslintrc.yml
+  let eslintrc = yaml('.eslintrc.yml')
+  let eslintrcJson = json('.eslintrc')
+  if (eslintrcJson.exists()) {
+    eslintrc.merge(eslintrcJson.get())
+  }
 
-	const pkg = packageJson();
+  if (!eslintrc.get('extends', '').startsWith(eslintPreset)) {
+    eslintrc.set('extends', eslintPreset)
+  }
+  if (eslintEnv) {
+    eslintrc.merge({ env: eslintEnv })
+  }
+  if (eslintRules) {
+    eslintrc.merge({ rules: eslintRules })
+  }
 
-	// TODO: Babel
-	// Not sure how to detect that we need it, checking for babel-core is not enough because
-	// babel-eslint is only needed for experimental features and Flow (this one is easy to test)
-	// Flow also needs this: https://github.com/gajus/eslint-plugin-flowtype
-	// if (pkg.get('devDependencies.babel-core')) {
-	// 	packages.push('babel-eslint');
-	// 	eslintrc.set('parser', 'babel-eslint');
-	// }
+  const pkg = packageJson()
 
-	// TypeScript
-	if (pkg.get('devDependencies.typescript')) {
-		const parser = 'typescript-eslint-parser';
-		packages.push(parser);
-		eslintrc.merge({
-			parser,
-			rules: eslintRules || {
-				// Disable rules not supported by TypeScript parser
-				// https://github.com/eslint/typescript-eslint-parser#known-issues
-				'no-undef': 0,
-				'no-unused-vars': 0,
-				'no-useless-constructor': 0,
-			},
-		});
-		exts = ' --ext .ts,.tsx';
-	}
+  // TODO: Babel
+  // Not sure how to detect that we need it, checking for babel-core is not enough because
+  // babel-eslint is only needed for experimental features and Flow (this one is easy to test)
+  // Flow also needs this: https://github.com/gajus/eslint-plugin-flowtype
+  // if (pkg.get('devDependencies.babel-core')) {
+  //   packages.push('babel-eslint');
+  //   eslintrc.set('parser', 'babel-eslint');
+  // }
 
-	eslintrc.save();
+  // TypeScript
+  if (pkg.get('devDependencies.typescript')) {
+    const parser = 'typescript-eslint-parser'
+    packages.push(parser)
+    eslintrc.merge({
+      parser,
+      rules: eslintRules || {
+        // Disable rules not supported by TypeScript parser
+        // https://github.com/eslint/typescript-eslint-parser#known-issues
+        'no-undef': 0,
+        'no-unused-vars': 0,
+        'no-useless-constructor': 0,
+      },
+    })
+    exts = ' --ext .ts,.tsx'
+  }
 
-	// .eslintignore
-	lines('.eslintignore')
-		.add(ignores)
-		.save();
+  eslintrc.save()
 
-	// .gitignore
-	lines('.gitignore')
-		.add(gitIgnores)
-		.save();
+  // .eslintignore
+  lines('.eslintignore')
+    .add(ignores)
+    .save()
 
-	// Keep custom extensions
-	const lintScript = pkg.getScript('lint', 'eslint') || pkg.getScript('test', 'eslint');
-	if (lintScript) {
-		const lintExts = getExtsFromCommand(lintScript);
-		if (lintExts && lintExts.toString() !== 'js') {
-			const extsPattern = lintExts.map(x => `.${x}`).join(',');
-			exts = ` --ext ${extsPattern}`;
-		}
-	}
+  // .gitignore
+  lines('.gitignore')
+    .add(gitIgnores)
+    .save()
 
-	pkg
-		// Remove existing JS linters
-		.removeScript(/^(lint:js|eslint|jshint|jslint)$/)
-		.removeScript('test', / (lint|lint:js|eslint|jshint|jslint)( |$)/) // npm run jest && npm run lint
-		.removeScript('test', /\beslint|jshint|jslint\b/) // jest && eslint
-		// Add lint script
-		.setScript('lint', 'eslint . --cache --fix' + exts)
-		// Add pretest script
-		.prependScript('pretest', 'npm run lint')
-		.save();
+  // Keep custom extensions
+  const lintScript = pkg.getScript('lint', 'eslint') || pkg.getScript('test', 'eslint')
+  if (lintScript) {
+    const lintExts = getExtsFromCommand(lintScript)
+    if (lintExts && lintExts.toString() !== 'js') {
+      const extsPattern = lintExts.map(x => `.${x}`).join(',')
+      exts = ` --ext ${extsPattern}`
+    }
+  }
 
-	// Dependencies
-	uninstall([...oldPackages, ...eslintObsoleteDependencies]);
-	install(packages);
+  pkg
+  // Remove existing JS linters
+    .removeScript(/^(lint:js|eslint|jshint|jslint)$/)
+    .removeScript('test', / (lint|lint:js|eslint|jshint|jslint)( |$)/) // npm run jest && npm run lint
+    .removeScript('test', /\beslint|jshint|jslint\b/) // jest && eslint
+  // Add lint script
+    .setScript('lint', 'eslint . --cache --fix' + exts)
+  // Add pretest script
+    .prependScript('pretest', 'npm run lint')
+    .save()
+
+  // Dependencies
+  uninstall([...oldPackages, ...eslintObsoleteDependencies])
+  install(packages, { dev: true })
 }
 
-task.description = 'Adds ESLint By @azm';
-module.exports = task;
+task.description = 'Adds ESLint By @azm'
+module.exports = task
