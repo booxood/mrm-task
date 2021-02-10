7d89b743ad3708287c5004e991915a05658a0563
booxood <liucw.mail@gmail.com>
2021-02-10 23:13:34 +0800

    feat: rename mrm-task-vscode

---
+++ packages/mrm-task-prettier/index.js
@@ -0,0 +1,51 @@
+const { json, install, packageJson } = require('mrm-core')
+
+const useYarn = true
+const usePrettier = {
+  'editor.formatOnSave': true,
+  'editor.defaultFormatter': 'esbenp.prettier-vscode',
+}
+
+const defaultPrettierOptions = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent', // as-needed, consistent, consistent
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
+
+function task (config) {
+  const settings = json('.vscode/settings.json', {})
+  settings.set(['editor.formatOnSave'], false)
+  settings.set(['editor.codeActionsOnSave'], {
+    'source.fixAll.eslint': true,
+  })
+  settings.set(['[css]'], usePrettier)
+  settings.set(['[less]'], usePrettier)
+  settings.set(['[json]'], usePrettier)
+  settings.set(['[yaml]'], usePrettier)
+  settings.set(['emmet.includeLanguages'], {
+    'javascript': 'javascriptreact',
+  })
+
+  const prettierrc = json('.prettierrc', defaultPrettierOptions)
+  prettierrc.save()
+
+  const pkg = packageJson()
+  pkg
+    .setScript('lint:style', 'prettier --write "src/**/*.{less,css}"')
+    .setScript('lint:config', 'prettier --write "*.{json,yaml}"')
+    .save()
+
+  install(['prettier'], { yarn: useYarn, dev: true })
+}
+
+task.description = 'Adds Prettier By @azm'
+module.exports = task
